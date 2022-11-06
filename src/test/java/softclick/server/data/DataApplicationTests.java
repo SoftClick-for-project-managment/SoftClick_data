a4fcf84d29c5da70004f01e0b105e97958b99f0f
Othmane Hanyf <othmane.hanyf@protonmail.com>
2022-11-06 16:01:46 +0100

    Bismi allah

---
+++ src/test/java/softclick/server/data/DataApplicationTests.java
@@ -0,0 +1,13 @@
+package softclick.server.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
