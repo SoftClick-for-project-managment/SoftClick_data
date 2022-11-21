ccf5366af92bea123a3c4d78b638fe0180981450
othmane <othmane.hanyf@protonmail.com>
2022-11-21 16:51:52 +0100

    Adding repositories for Task, Role and User

---
+++ src/main/java/softclick/server/data/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package softclick.server.data.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import softclick.server.data.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
