f4b3f895f8d08b75d73f9c0fd54fb2c835bc725d
othmane <othmane.hanyf@protonmail.com>
2022-11-22 22:47:32 +0100

    rm and add project files

--- src/main/java/softclick/server/data/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package softclick.server.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import softclick.server.data.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
--- src/main/java/softclick/server/data/repositories/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package softclick.server.data.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import softclick.server.data.entities.Task;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
