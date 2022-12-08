3537d7a8e1d6897579461adb8564a700b3fbc8c3
hindglioula123@gmail.com <hindglioula123@gmail.com>
2022-12-08 21:17:37 +0100

    delete @dataand add setters and getters (invoice + projects)

--- src/main/java/softclick/server/data/entities/Project.java
+++ src/main/java/softclick/server/data/entities/Project.java
@@ -13,13 +13,101 @@ import java.util.Set;
 
 @Entity
 @NoArgsConstructor
-@Data
+//@Data
 @Proxy(lazy = false)
 @Table(name = "project")
 public class Project implements Serializable, Comparable<Project> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idProject;
+
+    public Long getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(Long idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getNameProject() {
+        return nameProject;
+    }
+
+    public void setNameProject(String nameProject) {
+        this.nameProject = nameProject;
+    }
+
+    public String getDescriptionProject() {
+        return descriptionProject;
+    }
+
+    public void setDescriptionProject(String descriptionProject) {
+        this.descriptionProject = descriptionProject;
+    }
+
+    public Double getRevenueProject() {
+        return revenueProject;
+    }
+
+    public void setRevenueProject(Double revenueProject) {
+        this.revenueProject = revenueProject;
+    }
+
+    public Domain getDomainProjet() {
+        return domainProjet;
+    }
+
+    public void setDomainProjet(Domain domainProjet) {
+        this.domainProjet = domainProjet;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public Employee getChefProject() {
+        return chefProject;
+    }
+
+    public void setChefProject(Employee chefProject) {
+        this.chefProject = chefProject;
+    }
+
+    public Status getProjectStatus() {
+        return projectStatus;
+    }
+
+    public void setProjectStatus(Status projectStatus) {
+        this.projectStatus = projectStatus;
+    }
+
+    public Priority getProjectPriority() {
+        return projectPriority;
+    }
+
+    public void setProjectPriority(Priority projectPriority) {
+        this.projectPriority = projectPriority;
+    }
+
+    public Set<Invoice> getInvoices() {
+        return invoices;
+    }
+
+    public void setInvoices(Set<Invoice> invoices) {
+        this.invoices = invoices;
+    }
 
     @Column(name = "nameProject", nullable = false)
     private String nameProject;
