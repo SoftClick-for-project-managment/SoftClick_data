5d3e79319d14a4c6ab0f038c5ff6625746e55130
youssef <zahiYoussef>
2022-12-09 15:43:06 +0100

    changing entities

--- src/main/java/softclick/server/data/entities/Invoice.java
+++ src/main/java/softclick/server/data/entities/Invoice.java
@@ -78,45 +78,8 @@ public class Invoice implements Serializable {
         this.client=client;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getTotal() {
-        return total;
-    }
-
-    public void setTotal(String total) {
-        this.total = total;
-    }
-
-    public Client getClient() {
-        return client;
-    }
-
-    public void setClient(Client client) {
-        this.client = client;
-    }
-
-    public void setProject(Project project) {
-        this.project = project;
-    }
 
 
-    public Project getProject() {
-        return project;
-    }
+
 
 }
