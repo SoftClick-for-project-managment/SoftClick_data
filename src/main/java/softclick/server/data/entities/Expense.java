56f54d1dae28794623c521d9a118392e0dc25b7d
skyvaAz <elazharysoukaina@gmail.com>
2022-12-09 05:25:07 +0100

    EXPENSEdata

--- src/main/java/softclick/server/data/entities/Expense.java
+++ src/main/java/softclick/server/data/entities/Expense.java
@@ -2,6 +2,7 @@ package softclick.server.data.entities;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Proxy;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -10,6 +11,8 @@ import java.util.Date;
 @Entity
 @NoArgsConstructor
 @Data
+@Proxy(lazy=false)
+
 public class Expense implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,11 +26,13 @@ public class Expense implements Serializable {
     @ManyToOne
     @JoinColumn(name = "idTask")
     private Task task;
-    public Expense(Long amount , String typeExpense, Date date, ExpenseCategory expenseCategory){
+    public Expense(Long amount , String typeExpense, Date date, ExpenseCategory expenseCategory,Task task){
         this.amount=amount;
         this.typeExpense=typeExpense;
         this.expenseCategory=expenseCategory;
         this.date=date;
+        this.task=task;
+
     }
 
 
--- src/main/java/softclick/server/data/repositories/ExpenseRepository.java
+++ src/main/java/softclick/server/data/repositories/ExpenseRepository.java
@@ -1,7 +1,9 @@
 package softclick.server.data.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import softclick.server.data.entities.Expense;
+@Repository
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 }
