b98fea86544025b2163e5d2c2a1b5b95a3713b20
skyvaAz <elazharysoukaina@gmail.com>
2023-01-07 15:24:55 +0100

    updates

--- src/main/java/softclick/server/data/repositories/ExpenseRepository.java
+++ src/main/java/softclick/server/data/repositories/ExpenseRepository.java
@@ -3,6 +3,7 @@ package softclick.server.data.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softclick.server.data.entities.Expense;
+import softclick.server.data.entities.ExpenseCategory;
 import softclick.server.data.entities.Task;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     Expense getReferenceById(Long aLong);
 
     List<Expense> findByTypeExpense(String typeExpense);
-    List<Expense> findByExpenseCategory(String expenseCategory);
+    List<Expense> findByExpenseCategory(ExpenseCategory expenseCategory);
 
 
     Expense findByTask(Task task);
